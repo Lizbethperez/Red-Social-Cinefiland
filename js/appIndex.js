0044bf192d97a1654b91f9a3fe5a6968809d9865
Lizbethperez <lizbeth_perez_v@hotmail.com>
2018-06-25 03:22:42 -0500

    Se agrega funcionalidad con firebase al apartado del perfil de usuario

--- js/appIndex.js
+++ js/appIndex.js
@@ -59,14 +59,16 @@ document.addEventListener('DOMContentLoaded', function() {
           $("#peliculasVistas").append("<div class='movieElement'><span>"+PeliculasVistas[peli]+"</span></div>");
           $(".movieElement").addClass("collapsible-body");
         }
-  
+        $("#peliculasVistas").append("<div id='addmovie' class='col s4 friendElement'><a href='#add-watchedMovie' class='modal-trigger'> <i class='material-icons'>add_circle_outline </i>Anadir Pelicula </a></div>");
+          $(".movieElement").addClass("collapsible-body");
         var watchList = snap.child("watchList").val();
         for(peli in watchList)
         {
           $("#watchList").append("<div class='movieElement'><span>"+watchList[peli]+"</span></div>");
           $(".movieElement").addClass("collapsible-body");
         }
-  
+        $("#watchList").append("<div id='addmovie' class='col s4 friendElement'><a href='#add-movieWatchList' class='modal-trigger'> <i class='material-icons'>add_circle_outline </i>Anadir Pelicula </a></div>");
+          $(".movieElement").addClass("collapsible-body");
         var Amigos = snap.child("Amigos").val();
         for(amigo in Amigos)
         {
@@ -91,8 +93,17 @@ document.addEventListener('DOMContentLoaded', function() {
           var valueNameFriend = $("#icon_prefix2").val();
           $("#add-friend-btn").click(writeUserData(userName, valueNameFriend));
       */
+     document.getElementById("email").innerHTML = user.email;
+        var profileData = snap.child("ProfileData").val();
+        document.getElementById("name").innerHTML = profileData.Nombre;
+        document.getElementById("profile-age").innerHTML = profileData.Edad;
+        document.getElementById("profile-city").innerHTML = profileData.Ciudad;
+        document.getElementById("profile-job").innerHTML = profileData.Ocupacion;
+        document.getElementById("profile-status").innerHTML = profileData.estadoCivil;
         });
-        
+        $("#personal-data").append("<div id='addfriends' class='col s4 dataElement'><a href='#change-Info' class='modal-trigger'> <i class='material-icons'>add_circle_outline </i>Cambiar Infomacion </a></div>");
+        $(".dataElement").addClass("collapsible-body");
+
   });
   } ());
   function writeUserData() {
@@ -151,7 +162,7 @@ document.addEventListener('DOMContentLoaded', function() {
           $(".friendElement").addClass("collapsible-body");
   
         });
-        
+        location.href="index.html";
   });
   }
   
@@ -194,18 +205,213 @@ document.addEventListener('DOMContentLoaded', function() {
               firebase.database().ref().update({
                profileImage: downloadURL
               });
-  
   });
-  
   });
-  
-  
   }
-  
 
-  //FUNCIONALIDAD DE SECCIÓN DE COMENTARIOS (TEXT AREA)
+function Logout(){
+  firebase.auth().signOut().then(function() {
+    // Sign-out successful.
+          location.href="login.html";
+  }, function(error) {
+    // An error happened.
+  });
+}
 
-  function getCard() {
+function writeMovies() {
+  // Initialize Firebase
+   var config = {
+     apiKey: "AIzaSyAtdoJpgCIo3WvZb2vlXg48Qu-IE-Rwkkc",
+     authDomain: "mi-p-d4a76.firebaseapp.com",
+     databaseURL: "https://mi-p-d4a76.firebaseio.com",
+     projectId: "mi-p-d4a76",
+     storageBucket: "mi-p-d4a76.appspot.com",
+     messagingSenderId: "163277132167"
+   };
+   //firebase.initializeApp(config);
+   var cont = 0;
+   firebase.auth().onAuthStateChanged(firebaseUser =>{
+     if(firebaseUser){
+         console.log("logeado");
+         //location.href="index.html"
+     }else{
+         console.log("not loged in");
+         alert("your session expired, please log in again");
+         location.href="login.html";
+     }
+ 
+   var input_text = document.getElementById("watched-movie-value");
+   var input_text_value = input_text.value;
+ 
+     var user = firebase.auth().currentUser;
+     var userName = user.email.match(/^([^@]*)@/)[1];
+   var rootRef = firebase.database().ref(userName);
+   rootRef.on("value", snap => {
+   if(cont>0)
+     return;
+             var amigos = snap.child("PeliculasVistas").val();
+             var amigonumero= Object.keys(amigos).length+1;
+       var amigo_index= Object.keys(amigos).length;
+       var amigoindexValue = "titulo"+ amigo_index.toString();
+             var amigonext = "titulo"+ amigonumero.toString();
+       cont++;
+             firebase.database().ref(userName+"/PeliculasVistas").update({
+               [amigonext]: input_text_value
+             });
+     //return;
+       //    }
+ 
+     //var rootRef = firebase.database().ref().child(userName);
+     console.log(userName);
+      /*
+         $("#addmovie").remove();
+         //$("#Amigos").append("<div class='friendElement'><span>"+Amigos[amigo]+"</span></div>");
+         //$(".friendElement").addClass("collapsible-body row");
+         $("#peliculasVistas").append("<div class='addmovie'><div class='chip col s6'><img src='assets/images/user.jpg' alt='Contact Person'>"+input_text_value+"</div></div>");
+         $(".addmovie").addClass("collapsible-body"); 
+       
+       $("#peliculasVistas").append("<div id='addmovie' class='col s4 addmovie'><a href='#add-friend' class='modal-trigger'> <i class='material-icons'>add_circle_outline </i>Anadir Pelicula </a></div>");
+         $(".addmovie").addClass("collapsible-body");
+         */
+         location.href="index.html";
+       });
+       
+ });
+ }
+
+ function writeWatchlistItem() {
+  // Initialize Firebase
+   var config = {
+     apiKey: "AIzaSyAtdoJpgCIo3WvZb2vlXg48Qu-IE-Rwkkc",
+     authDomain: "mi-p-d4a76.firebaseapp.com",
+     databaseURL: "https://mi-p-d4a76.firebaseio.com",
+     projectId: "mi-p-d4a76",
+     storageBucket: "mi-p-d4a76.appspot.com",
+     messagingSenderId: "163277132167"
+   };
+   //firebase.initializeApp(config);
+   var cont = 0;
+   firebase.auth().onAuthStateChanged(firebaseUser =>{
+     if(firebaseUser){
+         console.log("logeado");
+         //location.href="index.html"
+     }else{
+         console.log("not loged in");
+         alert("your session expired, please log in again");
+         location.href="login.html";
+     }
+ 
+   var input_text = document.getElementById("value-watch-list");
+   var input_text_value = input_text.value;
+ 
+     var user = firebase.auth().currentUser;
+     var userName = user.email.match(/^([^@]*)@/)[1];
+   var rootRef = firebase.database().ref(userName);
+   rootRef.on("value", snap => {
+   if(cont>0)
+     return;
+      var watchlist = snap.child("watchList").val();
+      var pelinumero= Object.keys(watchlist).length+1;
+       var peli_index= Object.keys(watchlist).length;
+       var peliindexValue = "titulo"+ peli_index.toString();
+             var pelinext = "titulo"+ pelinumero.toString();
+       cont++;
+             firebase.database().ref(userName+"/watchList").update({
+               [pelinext]: input_text_value
+             });
+     //return;
+       //    }
+ 
+     //var rootRef = firebase.database().ref().child(userName);
+     console.log(userName);
+  /*
+         $("#addmovie").remove();
+         //$("#Amigos").append("<div class='friendElement'><span>"+Amigos[amigo]+"</span></div>");
+         //$(".friendElement").addClass("collapsible-body row");
+         $("#peliculasVistas").append("<div class='addmovie'><div class='chip col s6'><img src='assets/images/user.jpg' alt='Contact Person'>"+input_text_value+"</div></div>");
+         $(".addmovie").addClass("collapsible-body"); 
+       
+       $("#peliculasVistas").append("<div id='addmovie' class='col s4 addmovie'><a href='#add-friend' class='modal-trigger'> <i class='material-icons'>add_circle_outline </i>Anadir Pelicula </a></div>");
+         $(".addmovie").addClass("collapsible-body");
+ */
+       location.href="index.html";
+       });
+       
+ });
+ }
+
+ function changeProfileInfo() {
+  // Initialize Firebase
+   var config = {
+     apiKey: "AIzaSyAtdoJpgCIo3WvZb2vlXg48Qu-IE-Rwkkc",
+     authDomain: "mi-p-d4a76.firebaseapp.com",
+     databaseURL: "https://mi-p-d4a76.firebaseio.com",
+     projectId: "mi-p-d4a76",
+     storageBucket: "mi-p-d4a76.appspot.com",
+     messagingSenderId: "163277132167"
+   };
+   //firebase.initializeApp(config);
+   var cont = 0;
+   firebase.auth().onAuthStateChanged(firebaseUser =>{
+     if(firebaseUser){
+         console.log("logeado");
+         //location.href="index.html"
+     }else{
+         console.log("not loged in");
+         alert("your session expired, please log in again");
+         location.href="login.html";
+     }
+ 
+   var input_text1 = document.getElementById("cityValue");
+   var input_text_value1 = input_text1.value;
+   
+   var input_text2 = document.getElementById("ageValue");
+   var input_text_value2 = input_text2.value;
+
+   var input_text3 = document.getElementById("Ocupacion");
+   var input_text_value3 = input_text3.value;
+
+   var input_text4 = document.getElementById("estado-civil");
+   var input_text_value4 = input_text4.value;
+
+     var user = firebase.auth().currentUser;
+     var userName = user.email.match(/^([^@]*)@/)[1];
+   var rootRef = firebase.database().ref(userName);
+   rootRef.on("value", snap => {
+   if(cont>0)
+     return;
+      var watchlist = snap.child("ProfileData").val();
+       cont++;
+             firebase.database().ref(userName+"/ProfileData").update({
+               Ciudad: input_text_value1,
+               Edad: input_text_value2,
+               Ocupacion: input_text_value3,
+               estadoCivil: input_text_value4,
+             });
+     //return;
+       //    }
+ 
+     //var rootRef = firebase.database().ref().child(userName);
+     console.log(userName);
+  /*
+         $("#addmovie").remove();
+         //$("#Amigos").append("<div class='friendElement'><span>"+Amigos[amigo]+"</span></div>");
+         //$(".friendElement").addClass("collapsible-body row");
+         $("#peliculasVistas").append("<div class='addmovie'><div class='chip col s6'><img src='assets/images/user.jpg' alt='Contact Person'>"+input_text_value+"</div></div>");
+         $(".addmovie").addClass("collapsible-body"); 
+       
+       $("#peliculasVistas").append("<div id='addmovie' class='col s4 addmovie'><a href='#add-friend' class='modal-trigger'> <i class='material-icons'>add_circle_outline </i>Anadir Pelicula </a></div>");
+         $(".addmovie").addClass("collapsible-body");
+ */
+       location.href="index.html";
+       });
+       
+ });
+ }
+
+   //FUNCIONALIDAD DE SECCIÓN DE COMENTARIOS (TEXT AREA)
+
+   function getCard() {
     var inptComment = $("#comment").val();
     //console.log(inptComment);
 
@@ -214,8 +420,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
     addCommentCard(inptComment);
   }
-
-
 var card = '<div class="row">' + 
                  '<div>' +
                     '<div class="col s6 offset-s2">' +
